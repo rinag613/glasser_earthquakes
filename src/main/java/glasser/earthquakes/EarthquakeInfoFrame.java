158b854a49be5b17dc937db98ddf083a23ceceb6
rinag613 <108836650+rinag613@users.noreply.github.com>
2023-03-22 12:00:06 -0400

    removed unnesesary if else statements

--- src/main/java/glasser/earthquakes/EarthquakeInfoFrame.java
+++ src/main/java/glasser/earthquakes/EarthquakeInfoFrame.java
@@ -7,32 +7,29 @@ import java.awt.event.ActionListener;
 
 public class EarthquakeInfoFrame {
     Feature current = new Feature();
-    int numChosen = 0;
-    boolean tIsColored=false;
-    boolean QIsColored=false;
-    boolean newQuakeChosen = false;
+    boolean tIsColored = false;
+    boolean QIsColored = false;
 
     JPanel frame3;
     String[] titles = {"Magnitude", "Place", "Time", "Tsunami", "Coordinates"};
 
-    JButton[] displays = {new JButton(),new JButton(),new JButton(),new JButton(),new JButton()};
-    JButton[] titlesButtons = {new JButton(),new JButton(),new JButton(),new JButton(),new JButton()};
+    JButton[] displays = {new JButton(), new JButton(), new JButton(), new JButton(), new JButton()};
     JButton[] quakeButtons;
 
     String curTitle;
 
     public EarthquakeInfoFrame(FeatureCollection featureCollection) {
-        quakeButtons = new  JButton[featureCollection.features.length];
+        quakeButtons = new JButton[featureCollection.features.length];
         JFrame mainFrame = new JFrame("Wordle");
-        mainFrame.setLayout(new GridLayout(3,1));
+        mainFrame.setLayout(new GridLayout(3, 1));
 
         JPanel frame1 = new JPanel();
         frame1.setLayout(new GridLayout(1, featureCollection.features.length));
 
 
         for (int i = 0; i < featureCollection.features.length; i++) {
-            quakeButtons[i]= new JButton();
-            quakeButtons[i].setText("Quake " + (i+1));
+            quakeButtons[i] = new JButton();
+            quakeButtons[i].setText("Quake " + (i + 1));
             quakeButtons[i].setBackground(Color.GRAY);
             quakeButtons[i].setOpaque(true);
             frame1.add(quakeButtons[i]);
@@ -40,35 +37,24 @@ public class EarthquakeInfoFrame {
             quakeButtons[i].addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     mainFrame.requestFocus();
-                    if (QIsColored!=true) {
-                        current = featureCollection.features[ii];
-                        quakeButtons[ii].setBackground(Color.BLUE);
-                        quakeButtons[ii].setOpaque(true);
-                        QIsColored= true;
-                        newQuakeChosen = true;
-                    } else {
                         for (int j = 0; j < quakeButtons.length; j++) {
                             quakeButtons[j].setBackground(Color.GRAY);
                             quakeButtons[j].setOpaque(true);
                         }
                         for (int j = 0; j < titles.length; j++) {
                             displays[j].setText("");
-//                            titlesButtons[j].setBackground(Color.GRAY);
-//                            titlesButtons[j].setOpaque(true);
                         }
                         current = featureCollection.features[ii];
                         quakeButtons[ii].setBackground(Color.BLUE);
                         quakeButtons[ii].setOpaque(true);
-                        QIsColored= true;
-                        newQuakeChosen = true;
-                    }
+                        QIsColored = true;
                 }
             });
         }
         JPanel frame2 = new JPanel();
-        frame2.setLayout(new GridLayout(1,titles.length));
+        frame2.setLayout(new GridLayout(1, titles.length));
         frame3 = new JPanel();
-        frame3.setLayout(new GridLayout(1,titles.length));
+        frame3.setLayout(new GridLayout(1, titles.length));
 
         for (int i = 0; i < titles.length; i++) {
             JButton tButton = new JButton(titles[i]);
@@ -80,34 +66,24 @@ public class EarthquakeInfoFrame {
                 public void actionPerformed(ActionEvent e) {
                     mainFrame.requestFocus();
 
-                        tIsColored = true;
-                        curTitle = tButton.getText();
-//                        tButton.setBackground(Color.BLUE);
-//                        tButton.setOpaque(true);
-                        checkChosen();
-//                    if (newQuakeChosen==true) {
-//                        tIsColored=false;
-//                        curTitle = null;
-//                        tButton.setBackground(Color.GRAY);
-//                        tButton.setOpaque(true);
-//                        displays[ii].setText("");
-//                        newQuakeChosen= false;
-//                    }
+                    tIsColored = true;
+                    curTitle = tButton.getText();
+                    checkChosen();
 
                 }
             });
 
             for (int j = 0; j < displays.length; j++) {
                 frame3.add(displays[i]);
-                displays[i].setSize(30,300);
+                displays[i].setSize(30, 300);
                 displays[i].setBackground(Color.GRAY);
                 displays[i].setOpaque(true);
             }
 
         }
-        frame1.setSize(500,100);
-        frame2.setSize(500,100);
-        frame3.setSize(500,600);
+        frame1.setSize(500, 100);
+        frame2.setSize(500, 100);
+        frame3.setSize(500, 600);
         frame3.setVisible(true);
         frame2.setVisible(true);
         frame1.setVisible(true);
@@ -133,8 +109,9 @@ public class EarthquakeInfoFrame {
         } else if (curTitle.equals("Tsunami")) {
             displays[3].setText(String.valueOf(current.properties.tsunami));
         } else if (curTitle.equals("Coordinates")) {
-                displays[4].setText(String.valueOf(current.geometry.coordinates[0])+"" +(String.valueOf(current.geometry.coordinates[1])));
-            }
+            displays[4].setText("Lat: " + String.valueOf(current.geometry.coordinates[0])
+                            + "\n Long: " + (String.valueOf(current.geometry.coordinates[1])));
+        }
     }
 }
 
